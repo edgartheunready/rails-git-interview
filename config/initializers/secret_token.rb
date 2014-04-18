44a4c6780b66503a9171c052dbabb92f997f2bfd
Chris Keathley <spyc3r@gmail.com>
2014-04-18 16:57:59 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsGitInterview::Application.config.secret_key_base = 'bbad60a3cd087ea4a34c0c6efd3f77648bd622acd63e88da95934810f7bec702c1a4f9f474a09e2a0a2095e159e177b35d731bf6fe9277983af0d5cc7e824556'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsGitInterview::Application.config.session_store :cookie_store, key: '_rails-git-interview_session'
