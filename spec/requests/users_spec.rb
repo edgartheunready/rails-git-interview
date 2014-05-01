f926c187fd4118df3e21b59738541c82ece52041
Jared Menard <menard.jared@gmail.com>
2014-04-30 22:41:48 -0400

    adding one more test

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -7,4 +7,11 @@ describe "Users" do
       expect(page).to have_content("show")
     end
   end
+
+  describe "GET /users/search" do
+    it "works! (now write some real specs)" do
+      visit "/users/search"
+      expect(page).to have_content("search")
+    end
+  end
 end
